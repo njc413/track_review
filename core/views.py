86fa968e598154fc03fabfe91d6f5ff683ef5370
njc413 <njc413@aim.com>
2015-12-03 19:30:59 +0000

    test

--- core/views.py
+++ core/views.py
@@ -119,3 +119,12 @@ class UserDetailView(DetailView):
     slug_field = 'username'
     template_name = 'user/user_detail.html'
     context_object_name = 'user_in_view'
+
+    def get_context_data(self, **kwargs):
+        context = super(UserDetailView, self).get_context_data(**kwargs)
+        user_in_view = User.objects.get(username=self.kwargs['slug'])
+        reviews = Review.objects.filter(user=user_in_view)
+        context['reviews'] = reviews
+        replies = Reply.objects.filter(user=user_in_view)
+        context['replies'] = replies
+        return context
